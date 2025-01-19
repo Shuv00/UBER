188cd4a23830603ba29055be888e925f32d569bd
Shuv00 <shakil.ctg.a@gmail.com>
2025-01-19 22:51:11 +0600

    Refactor user registration logic and enhance user model validation

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,28 +1,22 @@
 const userModel = require('../models/user.model')
 const userService = require('../services/user.service')
-const {validationResult} = require('express-validator')
-module.exports.registerUser =  async (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array});
-    }
-    
-    
-  return {fullName,  email, password} = req.body;
-    console.log(req.body)
-    const hashedPassword =  await userModel.hashPassword(password);
-    password = hashedPassword;
-    const user = await userService.createUser(
-        
-            {
-                firstName:fullName.firstName,
-                lastName:fullName.firstName,
-                email,
-                password: hashedPassword,
-            }
-        
-    )
-    const token  =  user.generateAuthToken();
-   return res.status(201).json({token, user});
+const { validationResult } = require('express-validator')
 
+module.exports.registerUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { fullName, email, password } = req.body;
+    console.log(req.body);
+    const hashedPassword = await userModel.hashPassword(password);
+    const user = await userService.createUser({
+        firstName: fullName.firstName,
+        lastName: fullName.lastName,
+        email,
+        password: hashedPassword,
+    });
+    const token = user.generateAuthToken();
+    return res.status(201).json({ token, user });
 }
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,41 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const userSchema  = new mongoose.Schema({
-    fullName:{
-    firstName:{
+const userSchema = new mongoose.Schema({
+    fullName: {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: [3, "First name must be at least three characters"],
+        },
+        lastName: {
+            type: String,
+            minLength: [3, "Last name must be at least three characters"],
+        }
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
         type: String,
         required: true,
-        minLength: [3, "First name must be atleast three character"],
     },
-    lastName:{
+    socketId: {
         type: String,
-        minLength: [3, "First name must be atleast three character"],
-    }},
-    email:{
-        type:String,
-        unique:true,
-        required:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    socketId:{
-        type:String,
     }
-})
-userSchema.methods.generateAuthToken = function (){
-    const  token = jwt.sign({_id: this.id}, process.env.JWT_SECRET);
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this.id }, process.env.JWT_SECRET);
     return token;
-}
+};
 
-userSchema.methods.comparePassword = async function () {
-    return await bycrypt.compare(password, this.password);
-}
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
-userSchema.statics.hashPassword  = async function  () {
-    return await bycrypt.hash(password, 10);
-}   
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+};
 
 const userModel = mongoose.model('user', userSchema);
 
